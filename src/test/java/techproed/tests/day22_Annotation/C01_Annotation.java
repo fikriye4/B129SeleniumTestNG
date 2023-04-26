61901e17d9b7fbe5916088fc0e71a84be3411f9c
jessSchnere <iclalsener233@gmail.com>
2023-04-26 23:01:45 +0200

    Initial commit

---
+++ src/test/java/techproed/tests/day22_Annotation/C01_Annotation.java
@@ -0,0 +1,16 @@
+package techproed.tests.day22_Annotation;
+
+public class C01_Annotation {
+/*
+@Test:Testlerimizi yazmak icin kullandigimiz notasyon
+@BeforeSuite:Her bir test suitinden once bir kez calisisr
+@AfterSuite:Her bir test suitinden sonra bir kez calisir
+@BeforeTest:Her bir test oncesi bir kez calisir
+@AfteTest:Her bir test sonrasi bir kez calisir
+@BeforeClass:Her bir classtan once  bir kez calisir
+@AfteClass:Her birclasstan sonrasi bir kez calisir
+@BeforeMethod:Her bir test metotdundan once bir kez calisir
+@AfterMethod:Her bir test metotdundan sonra bir kez calisir
+
+ */
+}
